39268638cf1f45b328fbd2d8bf699786c98bed8d
bisrat2020 <wow.bisrat11@gmail.com>
2021-08-23 20:49:58 -0400

    add styles

--- netflix-front/src/App.js
+++ netflix-front/src/App.js
@@ -4,8 +4,15 @@ import request from './Assets/requests'
 function App() {
   return (
     <div className="App">
-      <Row title = "NeFlex original" fetchUrl = {request.fetchNetflixOriginals}/>
-      <Row title = "Trending Now"/>
+      <Row title = "NeFlex original" fetchUrl = {request.fetchNetflixOriginals} isLargeRow={true}/>
+      <Row title = "Trending Now" fetchUrl={request.fetchTrending}/>
+      <Row title = "Top Rated" fetchUrl={request.fetchTopRatedMovies}/>
+     
+      <Row title = "Action Movies" fetchUrl={request.fetchActionMovies}/>
+      <Row title = "Comedy Movies" fetchUrl={request.fetchComedyMovies}/>
+      <Row title = "Horror Movies" fetchUrl={request.fetchHorrorMovies}/>
+      <Row title = "Romance Movies" fetchUrl={request.fetchRomanceMovies}/>
+      
         
      
     </div>
--- netflix-front/src/Components/Row.jsx
+++ netflix-front/src/Components/Row.jsx
@@ -1,37 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import  '../Components/row.css'
+import '../Components/row.css'
 const imageBaseUrl = "https://image.tmdb.org/t/p/original/"
 
 
 function Row(props) {
     const [movies, setMovies] = useState([])
 
-    useEffect(()=>{
-        async function fetchData(){
+    useEffect(() => {
+        async function fetchData() {
             const request = await fetch(`https://api.themoviedb.org/3${props.fetchUrl}`)
-                .then(res=> res.json())
+                .then(res => res.json())
                 .then(data => {
                     const movieData = data.results;
                     setMovies(movieData)
                 })
-                .catch((err)=>{
+                .catch((err) => {
 
                 })
             return request
         }
         fetchData()
-    },[props.fetchUrl])
-    console.log(movies) 
+    }, [props.fetchUrl])
+    // console.log(movies) 
     return (
         <div className='row'>
-           <h2> {props.title}</h2> 
-           <div className="poster">
-                {movies.map((movie)=> (
-                    <img 
-                    className="poster"
-                    src={`${imageBaseUrl}${movie.poster_path}`} alt="" />
+            <h2> {props.title}</h2>
+            <div className="posters-container">
+                {movies.map((movie) => (
+                    <img
+                        key={movie.id}
+                        className={`poster ${props.isLargeRow && "large-poster"}`}
+                        src={`${imageBaseUrl}${
+                            props.isLargeRow ? 
+                            movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
                 ))}
-           </div>
+            </div>
         </div>
     )
 }
